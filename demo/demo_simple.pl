89f0f132f17bbea70c1561cd918cb964eb99871d
Damian Conway <damian@conway.org>
2000-12-08 07:16:36 +0000

    Import of DCONWAY/Lingua-Romana-Perligata-0.01 from CPAN.
    gitpan-cpan-distribution: Lingua-Romana-Perligata
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         DCONWAY/Lingua-Romana-Perligata-0.01.tar.gz
    gitpan-cpan-author:       DCONWAY
    gitpan-cpan-maturity:     released

---
+++ demo/demo_simple.pl
@@ -0,0 +1,20 @@
+use Lingua::Romana::Perligata
+# 'converte',
+# 'discribe'
+# 'investiga',
+;
+
+ute Tabs intra Text.
+
+inscribe label.
+dum nexto preincrescementum tum XCIX praestantiam fac sic
+       nextum tum addementum nextum tum unum oute.
+cis
+
+outere sic
+    scribe egresso haec admetamentum tum novumversum.
+    sic
+	scribe egresso next tum is inquementum tum biguttam
+		            tum lacunam tum hoc tum novumversum.
+    cis per quisque hoc in his fac.
+cis
